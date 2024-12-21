51cbecc87dcdbca01455cb35abc7f01b479b1b6e
Lục Sĩ Thân <lucsithanhd22@gmail.com>
2024-12-21 21:28:53 +0700

    Delete WEB_APP directory

--- WEB_APP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WEB_APP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
